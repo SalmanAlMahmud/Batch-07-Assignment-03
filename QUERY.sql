453e7aaee2f21fb8ce874fff1cc533720839c97d
Salman Al Mahmud <158093428+SalmanAlMahmud@users.noreply.github.com>
2026-06-15 02:04:25 +0600

    Add query to fetch user details by name pattern

--- QUERY.sql
+++ QUERY.sql
@@ -3,3 +3,8 @@ Query 1:
 select match_id,fixture,base_ticket_price from matches
 where tournament_category='Champions League' and match_status='Available'
 
+Query 2:
+
+select user_id,full_name,email from users
+where full_name like 'Tanvir%' or full_name like '%Haque'
+
